51472a52c2fb64db4b8626e6dbc35d7ade2ce113
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2026-03-30 09:52:09 +0300

    refactor: update Cypress configuration and improve login test formatting

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
+  watchForFileChanges: false,
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
